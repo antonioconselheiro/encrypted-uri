1ae0c30993a50e540804e296ef4780903fca00f9
António Conselheiro <canudos.sudestino@gmail.com>
2023-12-27 21:26:03 -0300

    codificando decode

--- encode-parser/index.ts
+++ encode-parser/index.ts
@@ -1,12 +1,47 @@
+
+const supportedAlgorithm = {
+  aes: 'aes',
+  salsa20: 'salsa20',
+  chacha: 'chacha',
+  xsalsa20: 'xsalsa20',
+  xchacha: 'xchacha',
+  poly1305: 'poly1305',
+  chacha8: 'chacha8',
+  chacha12: 'chacha12'
+};
+
+const AESOperationMode = {
+  cbc: 'cbc',
+  ecb: 'ecb',
+  ctr: 'ctr',
+  gcm: 'gcm',
+  siv: 'siv'
+};
+
+const AESPadding = {
+  pkcs7: 'pkcs7',
+  ansix923: 'ansix923',
+  iso10126: 'iso10126',
+  iso97971: 'iso97971',
+  zeropad: 'zeropad',
+  nopad: 'nopad'
+};
+
+const supportedAlgorithmList = Object.keys(supportedAlgorithm);
+const AESOperationModeList = Object.keys(AESOperationMode);
+
+type TEncryptedURISupportedAlgorithm = keyof typeof supportedAlgorithm;
+
 type TEncryptedURIDefaultParams = {
+  algorithm: TEncryptedURISupportedAlgorithm;
   cypher: string;
 }
 
 type TEncryptedAESParams = {
   algorithm: 'aes';
-  mode: 'cbc' | 'ecb' | 'ctr' | 'gcm' | 'siv';
+  mode: keyof typeof AESOperationMode;
   initializationVector: string;
-  padding: 'pkcs7' | 'ansix923' | 'iso10126' | 'iso97971' | 'zeropad' | 'nopad';
+  padding: keyof typeof AESPadding;
 }
 
 type TEncryptedNumberOnceParams = {
@@ -97,19 +132,63 @@ class URIEncrypted {
     config?: TEncryptedURIConfig
   ) {
     if (typeof content === 'string') {
-      this.decoded = this.decode(this.encoded = content);
+      const decoder = new URIEncryptedDecode();
+      this.decoded = decoder.decode(this.encoded = content);
       this.encoded = content;
     } else {
+      const encoder = new URIEncryptedEncode();
       this.decoded = content;
-      this.encoded = this.encode(content, config);
+      this.encoded = encoder.encode(content, config);
     }
   }
+}
 
-  private decode(content: string): TEncryptedURIResultset {
+class URIEncryptedDecode {
 
+  private readonly ENCRYPTED_URI_IDENTIFIER = /^encrypted:/;
+  private readonly QUERY_STRING_IDENTIFIER = /^?.*;+/;
+
+  decode(content: string): TEncryptedURIResultset {
+    const iterable = new IterableString(content);
+    const is = iterable.addCursor(this.ENCRYPTED_URI_IDENTIFIER);
+    if (!is) {
+      throw new InvalidURIEncrypted('missing \'encrypted\' keyword');
+    }
+
+    const identifySupportedAlgorithm = this.getSupportedAlgorithmMatcher();
+    const isSupported = iterable.addCursor(identifySupportedAlgorithm);
+    if (!isSupported) {
+      throw new AlgorithmNotSuported('algorithm not supported');
+    }
+
+    const identifyOperationMode = this.getIdentifyOperationModeMatcher();
+    const hasOperationMode = iterable.addCursor(identifyOperationMode);
+    if (hasOperationMode) {
+
+    }
+
+    
+    const queryString = iterable.addCursor(this.QUERY_STRING_IDENTIFIER);
+    if (queryString) {
+
+    }
+
+    
   }
 
-  private encode(content: TEncryptedURIResultset, config?: TEncryptedURIConfig): string {
+  private getSupportedAlgorithmMatcher(): RegExp {
+    const supported = supportedAlgorithmList.join('|');
+    return new RegExp(`^(${supported})`);
+  }
+
+  private getIdentifyOperationModeMatcher(): RegExp {
+    const operationMode = AESOperationModeList.join('|');
+    return new RegExp(`^(\/(${operationMode}))?`);
+  }
+}
+
+class URIEncryptedEncode {
+  encode(content: TEncryptedURIResultset, config?: TEncryptedURIConfig): string {
     const algorithm = this.encodeAlgorithmAndMode(content, config);
     const parameters = this.encodeParameters(content, config);
 
@@ -213,6 +292,128 @@ class URIEncrypted {
       alwaysIncludeMode,
       alwaysIncludeDefaultArgumentName
     }
+  }
+}
+
+export class IterableString {
+
+  private cursor = 0;
+  private readonly DEBUG_CHARS_PREVIEW = 100;
+
+  constructor(
+    private str: string
+  ) { }
+
+  get debugInfo(): string {
+    return String(this).substring(0, this.DEBUG_CHARS_PREVIEW);
+  }
+
+  get currenPosition(): number {
+    return this.cursor;
+  }
+
+  /**
+   * Return the string in it current cursor position
+   */
+  toString(): string {
+    return this.str.substring(this.cursor);
+  }
+
+  valueOf(): string {
+    return this.str.substring(this.cursor);
+  }
+
+  /**
+   * Return the original string
+   */
+  getOriginalString(): string {
+    return this.str;
+  }
+
+  /**
+   * Move the cursor and return the result
+   */
+  addCursor(param?: number | string | RegExp, autoTrimResult = true): string {
+    let result = '';
+    if (typeof param === 'number') {
+      result = this.addCursorNumeric(param);
+    } else if (typeof param === 'string') {
+      result = this.addCursorRegExp(new RegExp(param));
+    } else if (param instanceof RegExp) {
+      result = this.addCursorRegExp(param);
+    } else {
+      result = this.addCursorNumeric();
+    }
+
+    if (autoTrimResult) {
+      return result.trim();
+    } else {
+      return result;
+    }
+  }
+
+  /**
+   * Return result without move cursor
+   */
+  spy(param?: number | string | RegExp, autoTrimResult = true): string {
+    let result = '';
+    if (typeof param === 'number') {
+      result = this.spyNumeric(param);
+    } else if (typeof param === 'string') {
+      result = this.spyRegExp(new RegExp(param));
+    } else if (param instanceof RegExp) {
+      result = this.spyRegExp(param);
+    } else {
+      result = this.spyNumeric();
+    }
+
+    if (autoTrimResult) {
+      return result.trim();
+    } else {
+      return result;
+    }
+  }
+
+  private spyNumeric(howMuchMore = 1): string {
+    return this.str.substring(this.cursor, howMuchMore);
+  }
+
+  private spyRegExp(pattern: RegExp): string {
+    const matches = String(this).match(pattern);
+    return matches && matches.length && matches[0] || '';
+  }
+
+  /**
+   * Move the cursor that's iterating the string
+   */
+  private addCursorRegExp(pattern: RegExp): string {
+    if (!String(pattern).match(/^\/\^/)) {
+      throw new Error(
+        `all regexp used to move the cursor in the iterable string must start with ^. Entry regex: "${String(pattern)}"`
+      );
+    }
+
+    const match = this.spyRegExp(pattern);
+
+    return this.addCursorNumeric(match.length || 0);
+  }
+
+  /**
+   * If find an match with the regexp argument, it return the match and move the cursor
+   */
+  private addCursorNumeric(howMuchMore = 1): string {
+    const piece = this.spyNumeric(howMuchMore);
+    this.cursor += howMuchMore;
+
+    return piece;
+  }
+
+  endContent(): boolean {
+    return this.end() || !!this.spy(/^\s*$/, false);
+  }
+
+  end(): boolean {
+    return this.str.length <= this.cursor;
   }
 }
 
