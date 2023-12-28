87e2cc1590e2380c4d05d2a7b2ad29ad7b208cb2
António Conselheiro <canudos.sudestino@gmail.com>
2023-12-28 19:01:43 -0300

    incluindo testes unitários

--- encode-parser/index.test.ts
+++ encode-parser/index.test.ts
@@ -81,7 +81,7 @@ describe('decode uri with default values', () => {
   });
 });
 
-describe('encode uri with configs', () => {
+describe('encode uri with configs using default values', () => {
   it('encode with default config with default values', () => {
     expect(new URIEncrypted({
       algorithm: 'aes',
@@ -199,5 +199,103 @@ describe('encode uri with configs', () => {
       alwaysIncludePadding: true,
       alwaysIncludeDefaultArgumentName: true
     }).encoded).to.eql('encrypted:aes/cbc?iv=2345678wertyui&pad=pkcs7;en1e3kj3e31jn2algoritmgenerateddata')
+  });
+});
+
+describe('encode uri with customized values', () => {
+  it('encode aes/gcm with customized values', () => {
+    expect(new URIEncrypted({
+      algorithm: 'aes',
+      mode: 'gcm',
+      cypher: 'en1e3kj3e31jn2algoritmgenerateddata',
+      params: {
+        iv: '2345678wertyui',
+        pad: 'ecb'
+      }
+    }).encoded).to.eql('encrypted:aes/gcm?iv=2345678wertyui&pad=ecb;en1e3kj3e31jn2algoritmgenerateddata')
+  });
+
+  it('encode salsa20 with customized values', () => {
+    expect(new URIEncrypted({
+      algorithm: 'salsa20',
+      cypher: 'en1e3kj3e31jn2algoritmgenerateddata',
+      params: {
+        //  nonce
+        no: '871232183987132082713'
+      }
+    }).encoded).to.eql('encrypted:salsa20?no=871232183987132082713;en1e3kj3e31jn2algoritmgenerateddata')
+  });
+
+  it('encode xchacha with customized values', () => {
+    expect(new URIEncrypted({
+      algorithm: 'xchacha',
+      cypher: 'en1e3kj3e31jn2algoritmgenerateddata',
+      params: {
+        //  nonce
+        no: '871232183987132082713'
+      }
+    }).encoded).to.eql('encrypted:xchacha?no=871232183987132082713;en1e3kj3e31jn2algoritmgenerateddata')
+  });
+
+  it('encode chacha12 with customized values', () => {
+    expect(new URIEncrypted({
+      algorithm: 'chacha12',
+      cypher: 'en1e3kj3e31jn2algoritmgenerateddata',
+      params: {
+        //  nonce
+        no: '871232183987132082713'
+      }
+    }).encoded).to.eql('encrypted:chacha12?no=871232183987132082713;en1e3kj3e31jn2algoritmgenerateddata')
+  });
+});
+
+describe('decode uri with customized values', () => {
+  it('decode aes/gcm with customized values', () => {
+    expect(new URIEncrypted('encrypted:aes/gcm?iv=2345678wertyui&pad=ecb;en1e3kj3e31jn2algoritmgenerateddata').decoded)
+      .to.eql(new URIEncrypted({
+        algorithm: 'aes',
+        mode: 'gcm',
+        cypher: 'en1e3kj3e31jn2algoritmgenerateddata',
+        params: {
+          iv: '2345678wertyui',
+          pad: 'ecb'
+        }
+      }));
+  });
+
+  it('decode salsa20 with customized values', () => {
+    expect(new URIEncrypted('encrypted:salsa20?no=871232183987132082713;en1e3kj3e31jn2algoritmgenerateddata').decoded)
+      .to.eql(new URIEncrypted({
+        algorithm: 'salsa20',
+        cypher: 'en1e3kj3e31jn2algoritmgenerateddata',
+        params: {
+          //  nonce
+          no: '871232183987132082713'
+        }
+      }));
+  });
+
+  it('decode xchacha with customized values', () => {
+    expect(new URIEncrypted('encrypted:xchacha?no=871232183987132082713;en1e3kj3e31jn2algoritmgenerateddata').decoded)
+      .to.eql(new URIEncrypted({
+        algorithm: 'xchacha',
+        cypher: 'en1e3kj3e31jn2algoritmgenerateddata',
+        params: {
+          //  nonce
+          no: '871232183987132082713'
+        }
+      }));
+  });
+
+  it('decode chacha12 with customized values', () => {
+    expect(new URIEncrypted('encrypted:chacha12?no=871232183987132082713;en1e3kj3e31jn2algoritmgenerateddata').decoded)
+      .to.eql(new URIEncrypted({
+        algorithm: 'chacha12',
+        cypher: 'en1e3kj3e31jn2algoritmgenerateddata',
+        params: {
+          //  nonce
+          no: '871232183987132082713'
+        }
+      }));
   });
 });
