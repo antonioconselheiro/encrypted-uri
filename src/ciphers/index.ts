8c8f74de02d5d0735493f78d28cea1609380e303
António Conselheiro <canudos.sudestino@gmail.com>
2023-12-31 11:10:49 -0300

    modelando estrutura que permita extensibilidade dos algoritimos suportados pela biblioteca

--- src/ciphers/index.ts
+++ src/ciphers/index.ts
@@ -1,13 +1,22 @@
 import { URIEncryptedParser } from "@encrypted-uri/parser";
 
-const supportedAlgorithm = {
-  aes: 'aes',
+const supportedAlgorithmAes = {
+  aes: 'aes'
+};
+
+const supportedAlgorithmChacha = {
   salsa20: 'salsa20',
   chacha: 'chacha',
   xsalsa20: 'xsalsa20',
   xchacha: 'xchacha',
   chacha8: 'chacha8',
-  chacha12: 'chacha12'
+  chacha12: 'chacha12',
+  'xchacha20-poly1305': 'xchacha20-poly1305'
+};
+
+const supportedAlgorithm = {
+  ...supportedAlgorithmAes,
+  ...supportedAlgorithmChacha
 };
 
 const AESOperationMode = {
@@ -30,10 +39,11 @@ const AESPadding = {
 const supportedAlgorithmList = Object.keys(supportedAlgorithm);
 const AESOperationModeList = Object.keys(AESOperationMode);
 
+type TEncryptedURISupportedChachaAlgorithm = keyof typeof supportedAlgorithmChacha;
 type TEncryptedURISupportedAlgorithm = keyof typeof supportedAlgorithm;
 
 type TEncryptedURIDefaultParams = {
-  algorithm: TEncryptedURISupportedAlgorithm;
+  algorithm?: TEncryptedURISupportedAlgorithm;
 }
 
 type TEncryptedURIEncryptedDefaultParams = {
@@ -72,48 +82,119 @@ type TEncryptedAESParams = {
 }
 
 type TEncryptedChachaParams = {
-  algorithm: 'salsa20' | 'chacha' | 'xsalsa20' | 'xchacha' | 'chacha8' | 'chacha12';
+  algorithm?: TEncryptedURISupportedChachaAlgorithm;
   numberOnce: string;
 };
 
 type TEncryptedURIArguments = (TEncryptedAESParams | TEncryptedChachaParams);
 
-export class URIEncrypted {
-  static matcher(uri: string): boolean {
-    return URIEncryptedParser.matcher(uri);
-  }
-
-  static encode(params: TEncryptedURIEncryptedDefaultParams & TEncryptedURIArguments) {
-    return new URIEncryptedParser(params)
-  }
-
-  static encrypt(params: TEncryptedURIEncryptableDefaultParams & TEncryptedURIArguments) {
-    return new URIEncryptedEncrypter(params).encrypt();
-  }
-
-  static decrypt(encoded: string, key: string): string {
-    return new URIEncryptedDecrypter(encoded, key).decrypt();
-  }
+interface URIEncryptedEncrypter {
+  
+  encrypt(): string;
 }
 
-class URIEncryptedEncrypter {
+interface URIEncryptedDecrypter {
+  
+  decrypt(): string;
+}
+
+class URIEncryptedAESEncrypter implements URIEncryptedEncrypter {
   constructor(
-    private params: TEncryptedURIEncryptableDefaultParams & TEncryptedURIArguments
+    private params: TEncryptedURIEncryptableDefaultParams & TEncryptedAESParams
   ) { }
 
-  encrypt() {
-
+  encrypt(): string {
+    return '';
   }
 }
 
-class URIEncryptedDecrypter {
+class URIEncryptedAESDecrypter implements URIEncryptedDecrypter {
   constructor(
     private encoded: string,
     private key: string
-  ) {}
+  ) { }
 
   decrypt(): string {
     const parser = new URIEncryptedParser(this.encoded);
     const decoded = parser.decoded;
+
+    return '';
+  }
+}
+
+class URIEncryptedChachaEncrypter implements URIEncryptedEncrypter {
+  constructor(
+    private params: TEncryptedURIEncryptableDefaultParams & TEncryptedChachaParams
+  ) { }
+
+  encrypt(): string {
+    return '';
+  }
+}
+
+class URIEncryptedChachaDecrypter implements URIEncryptedDecrypter {
+  constructor(
+    private encoded: string,
+    private key: string
+  ) { }
+
+  decrypt(): string {
+    const parser = new URIEncryptedParser(this.encoded);
+    const decoded = parser.decoded;
+
+    return '';
+  }
+}
+
+export class URIEncrypted {
+
+  static readonly DEFAULT_ALGORITHM = 'aes';
+
+  static supportedAlgorithm: {
+    [algorithm in TEncryptedURISupportedAlgorithm | string]: [
+      { new (...args: any[]): URIEncryptedEncrypter },
+      { new (...args: any[]): URIEncryptedDecrypter }
+    ]
+  } = {
+    aes: [URIEncryptedAESEncrypter, URIEncryptedAESDecrypter],
+    salsa20: [URIEncryptedChachaEncrypter, URIEncryptedChachaDecrypter],
+    chacha: [URIEncryptedChachaEncrypter, URIEncryptedChachaDecrypter],
+    xsalsa20: [URIEncryptedChachaEncrypter, URIEncryptedChachaDecrypter],
+    xchacha: [URIEncryptedChachaEncrypter, URIEncryptedChachaDecrypter],
+    chacha8: [URIEncryptedChachaEncrypter, URIEncryptedChachaDecrypter],
+    chacha12: [URIEncryptedChachaEncrypter, URIEncryptedChachaDecrypter],
+    'xchacha20-poly1305': [URIEncryptedChachaEncrypter, URIEncryptedChachaDecrypter]
+  }
+
+  static matcher(uri: string): boolean {
+    return URIEncryptedParser.matcher(uri);
+  }
+
+  static encode(params: TEncryptedURIEncryptedDefaultParams & TEncryptedURIArguments): string {
+    return new URIEncryptedParser(params).encoded;
+  }
+
+  static encrypt(params: TEncryptedURIEncryptableDefaultParams & TEncryptedURIArguments) {
+    const [ encryptor ] = this.getAlgorithm(params.algorithm);
+    return new encryptor(params).encrypt();
+  }
+
+  static decrypt(uri: string, ...args: any[]): string {
+    const uriDecoded = new URIEncryptedParser(uri).decoded;
+    const [ , decryptor ] = this.getAlgorithm(uriDecoded.algorithm);
+    return new decryptor(...args).decrypt();
+  }
+
+  private static getAlgorithm(algorithm?: string): [
+    { new (...args: any[]): URIEncryptedEncrypter },
+    { new (...args: any[]): URIEncryptedDecrypter }
+  ] {
+    algorithm = algorithm || URIEncrypted.DEFAULT_ALGORITHM;
+    const [ encryptor, decryptor ] = this.supportedAlgorithm[algorithm] || [ null, null];
+    if (!encryptor && !decryptor) {
+      throw new Error(`Algorithm '${algorithm}' not supported`);
+    }
+
+    return [ encryptor, decryptor ];
   }
 }
