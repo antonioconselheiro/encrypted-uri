40e56ff4212d1f34dd5ad6abe21aad0e872b72f9
António Conselheiro <canudos.sudestino@gmail.com>
2024-01-11 14:58:58 -0300

    criando um esboço de como pretendo fazer as configurações do algoritimo de kdf

--- examples/ciphers-example/src/app/app.component.ts
+++ examples/ciphers-example/src/app/app.component.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { EncryptedURI } from '@encrypted-uri/core';
+import { pbkdf2 } from '@noble/hashes/pbkdf2';
+import { sha256 } from '@noble/hashes/sha256';
+import { randomBytes } from '@noble/hashes/utils';
 
 @Component({
   selector: 'app-root',
@@ -64,14 +67,24 @@ export class AppComponent {
     return this.submittedDecrypt && controls[property].errors[errorName] || false;
   }
 
+  private getKeyFromPasswordBasedKDF2(password: string, salt: Uint8Array): Uint8Array {
+    return pbkdf2(sha256, password, salt, { c: 1, dkLen: 32 });
+  }
+
   onEncryptSubmit(): void {
     if (this.encryptForm.valid) {
+      const salt = randomBytes(32);
       const raw = this.encryptForm.getRawValue();
-      if (raw.algorithm && raw.content && raw.key) {
+      if (raw.algorithm && raw.content && raw.password) {
         EncryptedURI.encrypt({
           algorithm: raw.algorithm,
           content: raw.content,
-          key: raw.key
+          password: raw.password,
+          kdf: {
+            hasher: 'sha256',
+            rounds: 1,
+            derivateKeyLength: 32
+          }
         }).then(uri => {
           this.generatedEncryptedURI = uri;
         });
@@ -82,9 +95,15 @@ export class AppComponent {
   onDecryptSubmit(): void {
     if (this.decryptForm.valid) {
       const raw = this.decryptForm.getRawValue();
-      if (raw.uri && raw.key) {
+      if (raw.uri && raw.password) {
         EncryptedURI
-          .decrypt(raw.uri, raw.key)
+          .decrypt(raw.uri, raw.password, {
+            kdf: {
+              hasher: sha256,
+              rounds: 1,
+              derivateKeyLength: 32
+            }
+          })
           .then(decrypted => this.decryptedContent = decrypted);
       }
     }
